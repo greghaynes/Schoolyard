557d23e572b578e61666787b664a1f164abd95fa
Gregory Haynes <greg@greghaynes.net>
2009-06-04 23:34:22 +0000

    Improved output on monte-carlo sphere integration

--- ComputationalPhysics/7_7.py
+++ ComputationalPhysics/7_7.py
@@ -1,7 +1,9 @@
-# -*- coding: utf-8 -*-
 import random
+import math
 
-count = 1000
+count = 10000 # number of points to use
+
+print 'Calculating area of a sphere with radius=1 using ' + str(count) + ' random points'
 hits = 0
 for i in xrange(count):
 	x = random.random()
@@ -10,4 +12,7 @@ for i in xrange(count):
 	dist = (x**2 + y**2 + z**2)**.5
 	if dist <= 1:
 		hits += 1
-print str(hits/float(count) * 8)
+hitrate = hits/float(count)
+print 'Percent inside sphere: ' + str(hits) + '/' + str(count) + ' = ' + str(hitrate) + '%'
+print 'Estimated area: ' + str(hitrate * 8)
+print 'Actial area: ' + str((4/3.)*math.pi)
